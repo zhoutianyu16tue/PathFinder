1fc953100a520416c3273778c495209fe9d2b6e7
Ubuntu <tianyu@tianyu.xzxpuix50qyeneduiwq3lqxqqb.ax.internal.cloudapp.net>
2018-05-02 09:16:54 +0000

    start adding more factors to the graph edges

--- julia_nb/osm/utils.jl
+++ julia_nb/osm/utils.jl
@@ -1,24 +1,41 @@
-map_rand_to_range(rand_float, a, b) = (rand_float - a) / (b - a)
+function weather_impact_factors(weather)
+    
+    # 0/1/2 -> sunny/rainy/snowy
+    
+    if weather == 0
+        return 0.0, 0.0
+    elseif weather == 1
+        return 0.1, 0.3
+    elseif weather == 2
+        return 0.3, 0.5
+    else
+        return 0.0, 0.0
+    end
+end
+
+map_rand_to_range(rand_float, a, b) = (b - a) * rand_float + a
 
 map_vehicles_to_slowing_factor(num_vehicles) = 1 - e ^ (-0.01num_vehicles)
 
-function generate_weights_with_factors(edge_len, speed_limit;
-                                        bad_weather=false, traffic_light=false,
-                                        num_vehicles=0)
+function generate_weights_with_factors(edge_info_dict)
+    
     # Units
     # edge_len: meters
     # speed_limit: m/s
-    # Assume that bad_weather slows a vehicle by 30% - 50%;
-    # Traffic light increases the edge weights by 10% - 30%;
     # so is number of vehicles on the edge;
+    # higher the centrality, lower the weight
+    # highway reduces the weight by 10% to 30%
     
-    weight = edge_len / speed_limit
+    weight = edge_info_dict[:segmentlen] / edge_info_dict[:speed]
     
-    traffic_light && (weight *= (1 + map_rand_to_range(rand(), 0.1, 0.3)))
+    weight *= (1 + map_vehicles_to_slowing_factor(edge_info_dict[:num_vehicles]))
     
-    bad_weather && (weight *= (1 + map_rand_to_range(rand(), 0.3, 0.5)))
+    weight *= (1 - edge_info_dict[:centrality])
     
-    weight *= (1 + map_vehicles_to_slowing_factor(num_vehicles))
+    edge_info_dict[:is_highway] && (weight *= (1 - map_rand_to_range(rand(), 0.1, 0.3)))
+    
+    factors = weather_impact_factors(edge_info_dict[:weather])
+    weight *= (1 + map_rand_to_range(rand(), factors[1], factors[2]))
     
     return weight
 end
